ca5ca9c786c36e9b6142b94c01af618dcccb99c9
Shubham Singh Bisht <shubham8318461615@gmail.com>
2024-01-25 19:04:12 +0530

    deployment2

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
     path('',include('ecommerceapp.urls')),
     path('auth/',include('authcart.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
